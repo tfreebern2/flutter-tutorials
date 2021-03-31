5ea99b63287815be7fc2321492395e3b667cf540
Tim Freebern <tim.freebern.ctr@teambespin.us>
2021-03-30 22:58:31 -0400

    Update Post Factor

--- stacked_arch_api_integration/start/lib/datamodel/post.dart
+++ stacked_arch_api_integration/start/lib/datamodel/post.dart
@@ -6,10 +6,7 @@ class Post {
 
   Post({this.userId, this.id, this.title, this.body});
 
-  Post.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
-    id = json['id'];
-    title = json['title'];
-    body = json['body'];
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(userId: json['userId'], id: json['id'], title: json['title'], body: json['body']);
   }
 }
